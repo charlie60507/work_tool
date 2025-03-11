eba9a04f001914cc9d2aa6c52c6c284d982f3be7
Charlie Yang <charlie.yang@gogox.com>
2025-03-11 15:22:51 +0800

    fix report starts with numbered list

--- lib/report_manager.py
+++ lib/report_manager.py
@@ -1,13 +1,13 @@
 
-ON_GOING_STATUS = {"1. Open", "3. In Progress", "4. Code Review", "5. Reopened"}
-COMPLETED_STATUS = {"5. Ready for QA", "7. Closed"}
+ON_GOING_STATUS = {"Open", "In Progress", "Code Review", "Reopened"}
+COMPLETED_STATUS = {"Ready for QA", "Closed"}
 
 def get_weekly_report(records):
     on_going_records = []
     completed_records = []
 
     for record in records:
-        status = record["status"]
+        status = record['status'].split('. ')[-1]
         formatted_record = f"- <{record['jiraUrl']}|{record['jiraId']}> {record['title']} `{status}`"
         if status in ON_GOING_STATUS:
             on_going_records.append(formatted_record)
